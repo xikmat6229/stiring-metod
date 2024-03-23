5b1893707558d36bd4c69caadcca371d0ce25fca
xikmatilla6229 <xikmatilla996@gmail.com>
2024-03-23 15:49:10 +0500

    stiring-metod

---
+++ java.js
@@ -0,0 +1,7 @@
+let a = prompt("Ismingizni kiriting:");
+let  b = prompt("Bironta harf kiriting:");
+if (a.includes(b)) {
+    alert("Kiritgan ismida bu harf bor!");
+} else {
+    alert("Kiritgan ismida bu harf yo'q!");
+}
